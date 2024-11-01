63a37bdcaafcc0cb3b02e18aa933ab271a2b3b5b
quantomo <quantomoenv@gmail.com>
2024-11-02 00:58:47 +0900

    feat: layout

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <nav>ここにナビゲーションバーを入れる</nav>
+    <slot />
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<script setup>
+definePageMeta({
+  layout: false
+})
+</script>
+<template>
+  <h1>About Page</h1>
+</template>
