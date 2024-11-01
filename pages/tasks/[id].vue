007ab678d6919cc550877c224169c4906dfdf126
quantomo <quantomoenv@gmail.com>
2024-11-02 03:35:11 +0900

    feat: fetch todos

--- pages/tasks/[id].vue
+++ pages/tasks/[id].vue
@@ -1,3 +1,20 @@
+<script setup>
+const route = useRoute();
+const id = route.params.id;
+const {
+  data: task,
+  error,
+  refresh,
+  status,
+} = await useLazyFetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+</script>
 <template>
-  <p>Task ID: {{ $route.params.id }}</p>
+  <button @click="refresh()">更新</button>
+  <p v-if="error">{{ error }}</p>
+  <p v-if="status === 'pending'">Loading ...</p>
+  <div v-else>
+    <p>Task ID: {{ task.id }}</p>
+    <p>{{ task.title }}</p>
+    <p>{{ task.completed }}</p>
+  </div>
 </template>
--- pages/tasks/index.vue
+++ pages/tasks/index.vue
@@ -1,3 +1,19 @@
+<script setup>
+const {
+  data: todos,
+  error,
+  refresh,
+  status,
+} = await useLazyFetch("https://jsonplaceholder.typicode.com/todos/");
+</script>
 <template>
-  <h1>Tasks Index</h1>
+  <h1>Tasks</h1>
+  <button @click="refresh()">更新</button>
+  <p v-if="error">{{ error }}</p>
+  <p v-if="status === 'pending'">Loading ...</p>
+  <ul v-else>
+    <li v-for="todo in todos" :key="todo.id">
+      <NuxtLink :to="`/tasks/${todo.id}`">{{ todo.title }}</NuxtLink>
+    </li>
+  </ul>
 </template>
